88d8b7bf3fcc1d3d160c3697b1f41f476b652ca1
Laurie <gloge@haathi-consulting.com>
2016-05-14 16:31:45 -0600

    basic list requirements, todolist and tasks completed and tested

--- todolist.rb
+++ todolist.rb
@@ -8,12 +8,45 @@ class TodoList
 	def title
 		@title 
 	end
+	def update_title(new_title)
+  	@title = new_title
+	end
 	def item
 		@items 
+	end
+	# add_item to the list
+	def add_item(new_item)
+	item = Item.new(new_item)
+	@items << item
+	end
+	# changing status on specific task
+  def update_item_status(index, status)
+        @items[index - 1].update_task_status(status)
+  end
+
+	# delete_item from the list
+	def delete_item(index)
+		@items.delete_at(index-1)
+	end 
+	def border
+	puts "-" * 50
+	end
+	def print_list_title
+        border
+        puts @title
+        border
+  end
+	def print_list
+		# print list title
+		print_list_title
+		@items.each_with_index do |task, index|
+			puts "#{task.print_task}"
+		end
 	end
 end
 	
 class Item
+
 	attr_reader :description, :completed_status
 
 	# Initialize item with a description and marked as
@@ -23,8 +56,54 @@ class Item
 	  @completed_status = false
 	end
 	def add_item(new_item)
-	  item = Item.new(new_item)
+	  @item = Item.new(new_item)
 	  @items.push(item)
 	end
+	# changing status of the task
+  def update_task_status(status)
+      @completed_status = status
+  end
+
+  # returning "true" if status is "done"
+  def completed?
+      @completed_status
+  end
+
+	def print_task
+       puts "Description: #{@description}" 
+  end
+
+  
+end
+
+class User
+	attr_reader :first_name, :last_name
+
+	# Initialize user 
+	# not complete
+	def initialize(first_name, last_name)
+	  @first_name = first_name
+	  @last_name = last_name
+	end
+
+	def add_user(new_user)
+		user = User.new(new_user)
+		@users.push(user)
+	end
+
+	def find_user(last_name)
+		@user.find_by(last_name)
+	end
+
+	def delete_user
+		@user.find_by(last_name)
+		@user.destroy
+	end
+
+	def name
+		"#{@first_name } #{@last_name}"
+	end
+
+
 end
 
--- udacitask.rb
+++ udacitask.rb
@@ -1,23 +1,38 @@
 require_relative 'todolist.rb'
 
 # Creates a new todo list
+list_one = TodoList.new("My First To Do List: ")
 
 # Add four new items
+list_one.add_item("get coffee")
+list_one.add_item("get wine")
+list_one.add_item("learn ruby")
+list_one.add_item("buy movie tickets")
 
 # Print the list
+list_one.print_list
 
 # Delete the first item
+list_one.delete_item(1)
 
 # Print the list
+list_one.print_list
 
 # Delete the second item
+list_one.delete_item(2)
 
 # Print the list
+list_one.print_list
 
 # Update the completion status of the first item to complete
+list_one.update_item_status(1, true)
 
 # Print the list
+list_one.print_list
 
 # Update the title of the list
+list_one.update_title("Friday List")
 
 # Print the list
+list_one.print_list
+
